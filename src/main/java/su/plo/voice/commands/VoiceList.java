e92975dc53e04646593fea4605e3b58b82f0ac6a
TheMrEngMan <68214507+TheMrEngMan@users.noreply.github.com>
2021-06-03 22:37:59 -0400

    Added vanish compatibility to /vlist Now players who can't be seen by the player who is executing the /vlist command won't be shown to them In other words, fixed an issue where /vlist could be used to expose vanished players who were using the mod

--- src/main/java/su/plo/voice/commands/VoiceList.java
+++ src/main/java/su/plo/voice/commands/VoiceList.java
@@ -5,9 +5,11 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.checkerframework.checker.units.qual.A;
 import su.plo.voice.PlasmoVoice;
 import su.plo.voice.socket.SocketServerUDP;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -19,9 +21,19 @@ public class VoiceList implements CommandExecutor {
                 .map(Player::getName)
                 .collect(Collectors.toList());
 
+        List<String> clientsToRemove = new ArrayList<>();
+        if(sender instanceof Player) {
+            for (Player currentPlayer : Bukkit.getOnlinePlayers()) {
+                if (!((Player) sender).canSee(currentPlayer)) {
+                    clientsToRemove.add(currentPlayer.getName());
+                }
+            }
+        }
+        clients.removeAll(clientsToRemove);
+
         sender.sendMessage(PlasmoVoice.getInstance().getMessagePrefix("list")
                 .replace("{count}", String.valueOf(clients.size()))
-                .replace("{online_players}", String.valueOf(Bukkit.getOnlinePlayers().size()))
+                .replace("{online_players}", String.valueOf(Bukkit.getOnlinePlayers().size() - clientsToRemove.size()))
                 .replace("{players}", String.join(", ", clients)));
         return true;
     }
