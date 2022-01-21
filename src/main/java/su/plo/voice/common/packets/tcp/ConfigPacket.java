3ed9ecdee387275bde42add306f2f1c74417e86e
Apehum <apehumchik@gmail.com>
2022-01-22 04:28:19 +0800

    Added setters to ConfigPacket

--- src/main/java/su/plo/voice/common/packets/tcp/ConfigPacket.java
+++ src/main/java/su/plo/voice/common/packets/tcp/ConfigPacket.java
@@ -2,6 +2,8 @@ package su.plo.voice.common.packets.tcp;
 
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteArrayDataOutput;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import su.plo.voice.common.packets.Packet;
 
 import java.io.IOException;
@@ -9,6 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
 public class ConfigPacket implements Packet {
     private int sampleRate;
     private final List<Integer> distances;
@@ -20,45 +24,6 @@ public class ConfigPacket implements Packet {
 
     public ConfigPacket() {
         this.distances = new ArrayList<>();
-    }
-
-    public ConfigPacket(int sampleRate, List<Integer> distances, int defaultDistance, int maxPriorityDistance,
-                        boolean disableVoiceActivation, int fadeDivisor, int priorityFadeDivisor) {
-        this.sampleRate = sampleRate;
-        this.distances = distances;
-        this.defaultDistance = defaultDistance;
-        this.maxPriorityDistance = maxPriorityDistance;
-        this.fadeDivisor = fadeDivisor;
-        this.priorityFadeDivisor = priorityFadeDivisor;
-        this.disableVoiceActivation = disableVoiceActivation;
-    }
-
-    public int getSampleRate() {
-        return sampleRate;
-    }
-
-    public List<Integer> getDistances() {
-        return distances;
-    }
-
-    public int getDefaultDistance() {
-        return defaultDistance;
-    }
-
-    public int getFadeDivisor() {
-        return fadeDivisor;
-    }
-
-    public int getPriorityFadeDivisor() {
-        return priorityFadeDivisor;
-    }
-
-    public int getMaxPriorityDistance() {
-        return maxPriorityDistance;
-    }
-
-    public boolean isDisableVoiceActivation() {
-        return disableVoiceActivation;
     }
 
     @Override
