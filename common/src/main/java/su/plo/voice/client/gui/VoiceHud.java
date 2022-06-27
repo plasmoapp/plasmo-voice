18513f2d7ac3f64296936899e190d6678cf7e6af
Apehum <apehumchik@gmail.com>
2022-06-27 08:59:23 +0800

    separate functions for VoiceHud icon render

--- common/src/main/java/su/plo/voice/client/gui/VoiceHud.java
+++ common/src/main/java/su/plo/voice/client/gui/VoiceHud.java
@@ -12,78 +12,68 @@ public class VoiceHud {
     private final Minecraft client = Minecraft.getInstance();
 
     public void render() {
-        if (!VoiceClient.isConnected()) {
-            return;
-        }
+        if (!VoiceClient.isConnected()) return;
 
         final Player player = client.player;
-        final Gui inGameHud = client.gui;
-        final PoseStack matrixStack = new PoseStack();
-
         if (player == null) return;
 
         if (VoiceClient.socketUDP.isTimedOut()) {
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1F, 1F, 1F, 1F);
-            RenderSystem.setShaderTexture(0, VoiceClient.ICONS);
-
-            inGameHud.blit(matrixStack,
-                    VoiceClient.getClientConfig().micIconPosition.get().getX(client),
-                    VoiceClient.getClientConfig().micIconPosition.get().getY(client),
-                    0,
-                    16,
-                    16,
-                    16);
+            renderConnectionError();
             return;
         }
 
         if (VoiceClient.getClientConfig().speakerMuted.get()) {
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1F, 1F, 1F, 1F);
-            RenderSystem.setShaderTexture(0, VoiceClient.ICONS);
-
-            inGameHud.blit(matrixStack,
-                    VoiceClient.getClientConfig().micIconPosition.get().getX(client),
-                    VoiceClient.getClientConfig().micIconPosition.get().getY(client),
-                    80,
-                    0,
-                    16,
-                    16);
-        } else if (VoiceClient.getClientConfig().microphoneMuted.get() || !VoiceClient.recorder.isAvailable()
-                || VoiceClient.getServerConfig().getMuted().containsKey(player.getUUID())) {
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1F, 1F, 1F, 1F);
-            RenderSystem.setShaderTexture(0, VoiceClient.ICONS);
-
-            inGameHud.blit(matrixStack,
-                    VoiceClient.getClientConfig().micIconPosition.get().getX(client),
-                    VoiceClient.getClientConfig().micIconPosition.get().getY(client),
-                    16,
-                    0,
-                    16,
-                    16);
+            renderSpeakerMuted();
+        } else if (VoiceClient.getClientConfig().microphoneMuted.get()
+                || !VoiceClient.recorder.isAvailable()
+                || VoiceClient.getServerConfig().getMuted().containsKey(player.getUUID())
+        ) {
+            renderMicrophoneMuted();
         } else if (VoiceClient.isSpeaking()) {
-            RenderSystem.setShader(GameRenderer::getPositionTexShader);
-            RenderSystem.setShaderColor(1F, 1F, 1F, 1F);
-            RenderSystem.setShaderTexture(0, VoiceClient.ICONS);
-
             if (VoiceClient.isSpeakingPriority()) {
-                inGameHud.blit(matrixStack,
-                        VoiceClient.getClientConfig().micIconPosition.get().getX(client),
-                        VoiceClient.getClientConfig().micIconPosition.get().getY(client),
-                        16,
-                        16,
-                        16,
-                        16);
+                renderPrioritySpeaking();
             } else {
-                inGameHud.blit(matrixStack,
-                        VoiceClient.getClientConfig().micIconPosition.get().getX(client),
-                        VoiceClient.getClientConfig().micIconPosition.get().getY(client),
-                        0,
-                        0,
-                        16,
-                        16);
+                renderSpeaking();
             }
         }
+    }
+
+    private void renderConnectionError() {
+        renderIcon(0, 16);
+    }
+
+    private void renderSpeakerMuted() {
+        renderIcon(80, 0);
+    }
+
+    private void renderMicrophoneMuted() {
+        renderIcon(16, 0);
+    }
+
+    private void renderSpeaking() {
+        renderIcon(0, 0);
+    }
+
+    private void renderPrioritySpeaking() {
+        renderIcon(16, 16);
+    }
+
+    private void renderIcon(int x, int z) {
+        final Gui inGameHud = client.gui;
+        final PoseStack matrixStack = new PoseStack();
+
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1F, 1F, 1F, 1F);
+        RenderSystem.setShaderTexture(0, VoiceClient.ICONS);
+
+        inGameHud.blit(
+                matrixStack,
+                VoiceClient.getClientConfig().micIconPosition.get().getX(client),
+                VoiceClient.getClientConfig().micIconPosition.get().getY(client),
+                x,
+                z,
+                16,
+                16
+        );
     }
 }
