144e4f7f3a1a3bb786b1669538152ffad817ff34
Apehum <apehumchik@gmail.com>
2022-08-11 05:18:08 +0800

    refactor(api): use byte[] instead of Params in encryption

--- api/common/src/main/java/su/plo/voice/api/encryption/EncryptionManager.java
+++ api/common/src/main/java/su/plo/voice/api/encryption/EncryptionManager.java
@@ -1,13 +1,12 @@
 package su.plo.voice.api.encryption;
 
 import org.jetbrains.annotations.NotNull;
-import su.plo.voice.api.util.Params;
 
 import java.util.Collection;
 
 // todo: doc
 public interface EncryptionManager {
-    @NotNull Encryption create(@NotNull String name, @NotNull Params params);
+    @NotNull Encryption create(@NotNull String name, byte[] data);
 
     void register(@NotNull EncryptionSupplier supplier);
 
--- api/common/src/main/java/su/plo/voice/api/encryption/EncryptionSupplier.java
+++ api/common/src/main/java/su/plo/voice/api/encryption/EncryptionSupplier.java
@@ -1,10 +1,9 @@
 package su.plo.voice.api.encryption;
 
 import org.jetbrains.annotations.NotNull;
-import su.plo.voice.api.util.Params;
 
 public interface EncryptionSupplier {
-    @NotNull Encryption create(@NotNull Params params);
+    @NotNull Encryption create(byte[] data);
 
     @NotNull String getName();
 }
