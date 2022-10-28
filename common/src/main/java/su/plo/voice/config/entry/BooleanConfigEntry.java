c8175e42f34d1bd65eca479f99595f648e49b23b
Apehum <apehumchik@gmail.com>
2022-10-28 16:21:02 +0800

    chore(common): make BooleanConfigEntry final

--- common/src/main/java/su/plo/voice/config/entry/BooleanConfigEntry.java
+++ common/src/main/java/su/plo/voice/config/entry/BooleanConfigEntry.java
@@ -2,7 +2,7 @@ package su.plo.voice.config.entry;
 
 import su.plo.config.entry.ConfigEntry;
 
-public class BooleanConfigEntry extends ConfigEntry<Boolean> {
+public final class BooleanConfigEntry extends ConfigEntry<Boolean> {
 
     public BooleanConfigEntry(Boolean defaultValue) {
         super(defaultValue);
