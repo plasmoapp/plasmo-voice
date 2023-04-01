478ee8aa99a8f40f6e53a1d1ad818f7969533f7a
mani1232 <perederkoura@gmail.com>
2023-04-01 15:59:58 +0200

    Folia and paper-plugin.yml support

---
+++ server/paper/src/main/kotlin/su/plo/voice/paper/PaperVoiceBootstrap.kt
@@ -0,0 +1,9 @@
+package su.plo.voice.paper
+
+import io.papermc.paper.plugin.bootstrap.PluginBootstrap
+import io.papermc.paper.plugin.bootstrap.PluginProviderContext
+
+class PaperVoiceBootstrap : PluginBootstrap {
+    override fun bootstrap(context: PluginProviderContext) {
+    }
+}
