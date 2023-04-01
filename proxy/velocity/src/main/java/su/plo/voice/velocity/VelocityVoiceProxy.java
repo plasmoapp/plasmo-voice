d05a22e5476a3287c28bdc0d8182bb5bc8be1010
Apehum <apehumchik@gmail.com>
2023-04-01 08:42:37 +0800

    feat(proxy): add LuckPerms optional dependency in Velocity

--- proxy/velocity/src/main/java/su/plo/voice/velocity/VelocityVoiceProxy.java
+++ proxy/velocity/src/main/java/su/plo/voice/velocity/VelocityVoiceProxy.java
@@ -5,6 +5,7 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.permission.Tristate;
+import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
 import com.velocitypowered.api.proxy.Player;
@@ -28,7 +29,10 @@ import java.nio.file.Path;
         id = "plasmovoice",
         name = "PlasmoVoice",
         version = BuildConstants.VERSION,
-        authors = "Apehum"
+        authors = "Apehum",
+        dependencies = {
+                @Dependency(id = "luckperms", optional = true)
+        }
 )
 public final class VelocityVoiceProxy extends BaseVoiceProxy {
 
