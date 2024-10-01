281eaac748942bbd9e7e19fa0e4f46b528d14c9b
Apehum <36326454+Apehum@users.noreply.github.com>
2024-10-01 11:26:24 +0800

    build(client): add 1.19.3 and soften some version bounds

--- client/changelog.md
+++ client/changelog.md
@@ -6,3 +6,7 @@ Versions 2.0.x and 2.1.x are protocol-compatible,
 so there’s no need to worry if the server hasn't been updated to 2.1.x.
 
 ### Changes in 2.1.1
+- Reintroduce build for 1.19.3
+- Soften Minecraft version bounds on some versions:
+    - 1.20.4 now allows 1.20.2, 1.20.3 and 1.20.4
+    - 1.19.2 now allows 1.19, 1.19.1 and 1.19.2
--- client/root.gradle.kts
+++ client/root.gradle.kts
@@ -19,6 +19,9 @@ preprocess {
     val forge11904 = createNode("1.19.4-forge", 11904, "official")
     val fabric11904 = createNode("1.19.4-fabric", 11904, "official")
 
+    val forge11903 = createNode("1.19.3-forge", 11903, "official")
+    val fabric11903 = createNode("1.19.3-fabric", 11903, "official")
+
     val forge11902 = createNode("1.19.2-forge", 11902, "official")
     val fabric11902 = createNode("1.19.2-fabric", 11902, "official")
 
@@ -44,8 +47,11 @@ preprocess {
     // fabric 1.19.4 main project
     forge11904.link(fabric11904)
 
-    fabric11902.link(fabric11904, file("1.19.2-1.19.3.txt"))
-    forge11902.link(forge11904, file("1.19.2-1.19.3.txt"))
+    fabric11903.link(fabric11904)
+    forge11903.link(forge11904)
+
+    fabric11902.link(fabric11903, file("1.19.2-1.19.3.txt"))
+    forge11902.link(forge11903, file("1.19.2-1.19.3.txt"))
 
     fabric11802.link(fabric11902)
     forge11802.link(forge11902, file("1.18.2-1.19.2.txt"))
