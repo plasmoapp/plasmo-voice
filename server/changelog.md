96d8a7649207542ac582eae702bff86a60d9e11a
Apehum <apehum@proton.me>
2025-03-19 08:09:56 +0800

    fix(paper): broadcast disconnect packet in PostPlayerHideEvent and don't send it to the hidden player

--- server/changelog.md
+++ server/changelog.md
@@ -1,1 +1,2 @@
 ### Changes in 2.1.4
+- Fixed SuperVanish `/hide` causes hidden client to disconnect.
--- server/paper/src/main/kotlin/su/plo/voice/paper/integration/SuperVanishIntegration.kt
+++ server/paper/src/main/kotlin/su/plo/voice/paper/integration/SuperVanishIntegration.kt
@@ -1,21 +1,26 @@
 package su.plo.voice.paper.integration
 
-import de.myzelyam.api.vanish.PlayerHideEvent
+import de.myzelyam.api.vanish.PostPlayerHideEvent
 import de.myzelyam.api.vanish.PostPlayerShowEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import su.plo.voice.api.server.PlasmoVoiceServer
+import su.plo.voice.proto.packets.tcp.clientbound.PlayerDisconnectPacket
 
 class SuperVanishIntegration(
-    private val voiceServer: PlasmoVoiceServer
+    private val voiceServer: PlasmoVoiceServer,
 ) : Listener {
 
     @EventHandler
-    fun onPlayerHide(event: PlayerHideEvent) {
+    fun onPlayerHide(event: PostPlayerHideEvent) {
         val player = voiceServer.playerManager.getPlayerByInstance(event.player)
         if (!player.hasVoiceChat()) return
 
-        voiceServer.tcpPacketManager.broadcastPlayerDisconnect(player)
+        voiceServer.tcpPacketManager.broadcast(
+            PlayerDisconnectPacket(player.instance.uuid),
+        ) { other ->
+            other.instance.uuid != player.instance.uuid && !other.instance.canSee(player.instance)
+        }
     }
 
     @EventHandler
