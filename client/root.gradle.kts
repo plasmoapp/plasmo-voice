41f724930a59afb60f3a3b9f4a3a28988de3dc2a
Apehum <apehum@proton.me>
2025-07-01 04:22:42 +0800

    feat(client): build for 1.21.7 neoforge

--- client/root.gradle.kts
+++ client/root.gradle.kts
@@ -7,6 +7,8 @@ group = "$group.client-root"
 preprocess {
     strictExtraMappings.set(false)
 
+    val neoForge12107 = createNode("1.21.7-neoforge", 12106, "official")
+
     val neoForge12106 = createNode("1.21.6-neoforge", 12106, "official")
     val fabric12106 = createNode("1.21.6-fabric", 12106, "official")
 
@@ -46,6 +48,8 @@ preprocess {
 
     val forge11605 = createNode("1.16.5-forge", 11605, "official")
     val fabric11605 = createNode("1.16.5-fabric", 11605, "official")
+
+    neoForge12107.link(neoForge12106)
 
     fabric12106.link(fabric12105, file("1.21.6-1.21.5.txt"))
     neoForge12106.link(neoForge12105, file("1.21.6-1.21.5.txt"))
