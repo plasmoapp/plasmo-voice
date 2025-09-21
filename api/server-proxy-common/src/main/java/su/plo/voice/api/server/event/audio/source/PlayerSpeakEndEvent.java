eb2bc915e565cd153d20491542c8de8fef03f451
Apehum <apehum@proton.me>
2025-09-22 07:08:06 +0800

    docs: clarify cancel state of PlayerSpeakEvent and PlayerSpeakEndEvent

--- api/server-proxy-common/src/main/java/su/plo/voice/api/server/event/audio/source/PlayerSpeakEndEvent.java
+++ api/server-proxy-common/src/main/java/su/plo/voice/api/server/event/audio/source/PlayerSpeakEndEvent.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 import su.plo.voice.api.event.EventCancellableBase;
+import su.plo.voice.api.event.EventPriority;
+import su.plo.voice.api.event.EventSubscribe;
 import su.plo.voice.api.server.audio.capture.ServerActivation;
 import su.plo.voice.api.server.event.audio.capture.PlayerServerActivationEndEvent;
 import su.plo.voice.api.server.player.VoicePlayer;
@@ -11,6 +13,15 @@ import su.plo.voice.proto.packets.tcp.serverbound.PlayerAudioEndPacket;
 
 /**
  * This event is fired when the {@link PlayerAudioEndPacket} is received.
+ * <br/>
+ * This event is cancelled if activation was handled successfully by
+ * {@link ServerActivation#onPlayerActivationEnd(ServerActivation.PlayerActivationEndListener)}.
+ * <br/>
+ * By default, {@link EventSubscribe} ignores cancelled events,
+ * so if you want to listen for this event, make sure to set {@link EventSubscribe#ignoreCancelled()} to {@code false}
+ * or {@link EventSubscribe#priority()} to {@link EventPriority#LOWEST} to handle it before it handled by the internal listener.
+ * <br/>
+ * Or use activation-related events, they're not cancelable.
  *
  * @see PlayerServerActivationEndEvent
  */
--- api/server-proxy-common/src/main/java/su/plo/voice/api/server/event/audio/source/PlayerSpeakEvent.java
+++ api/server-proxy-common/src/main/java/su/plo/voice/api/server/event/audio/source/PlayerSpeakEvent.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.jetbrains.annotations.NotNull;
 import su.plo.voice.api.event.EventCancellableBase;
+import su.plo.voice.api.event.EventPriority;
+import su.plo.voice.api.event.EventSubscribe;
 import su.plo.voice.api.server.audio.capture.ServerActivation;
 import su.plo.voice.api.server.event.audio.capture.PlayerServerActivationEvent;
 import su.plo.voice.api.server.event.audio.capture.PlayerServerActivationStartEvent;
@@ -12,6 +14,15 @@ import su.plo.voice.proto.packets.udp.serverbound.PlayerAudioPacket;
 
 /**
  * This event is fired when the {@link PlayerAudioPacket} is received.
+ * <br/>
+ * This event is cancelled if activation was handled successfully by
+ * {@link ServerActivation#onPlayerActivation(ServerActivation.PlayerActivationListener)}.
+ * <br/>
+ * By default, {@link EventSubscribe} ignores cancelled events,
+ * so if you want to listen for this event, make sure to set {@link EventSubscribe#ignoreCancelled()} to {@code false}
+ * or {@link EventSubscribe#priority()} to {@link EventPriority#LOWEST} to handle it before it handled by the internal listener.
+ * <br/>
+ * Or use activation-related events, they're not cancelable.
  *
  * @see PlayerServerActivationEvent
  * @see PlayerServerActivationStartEvent
