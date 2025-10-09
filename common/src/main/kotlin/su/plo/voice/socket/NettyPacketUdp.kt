e016571fdb761dd1e85240ed8750590c4cb70e2a
Apehum <apehum@proton.me>
2025-10-10 00:34:17 +0800

    fix: implement ReferenceCounted for NettyPacketUdp directly instead of using kotlin delegate

--- common/src/main/kotlin/su/plo/voice/socket/NettyPacketUdp.kt
+++ common/src/main/kotlin/su/plo/voice/socket/NettyPacketUdp.kt
@@ -1,11 +1,25 @@
 package su.plo.voice.socket
 
-import io.netty.buffer.ByteBufHolder
 import io.netty.channel.socket.DatagramPacket
+import io.netty.util.ReferenceCounted
 import su.plo.voice.proto.packets.udp.PacketUdp
 
 data class NettyPacketUdp(
     val datagramPacket: DatagramPacket,
     val packetUdp: PacketUdp,
-) : ByteBufHolder by datagramPacket
+) : ReferenceCounted {
+    override fun refCnt() = datagramPacket.refCnt()
+
+    override fun retain() = apply { datagramPacket.retain() }
+
+    override fun retain(increment: Int) = apply { datagramPacket.retain(increment) }
+
+    override fun touch() = apply { datagramPacket.touch() }
+
+    override fun touch(hint: Any?) = apply { datagramPacket.touch(hint) }
+
+    override fun release() = datagramPacket.release()
+
+    override fun release(decrement: Int) = datagramPacket.release(decrement)
+}
 
