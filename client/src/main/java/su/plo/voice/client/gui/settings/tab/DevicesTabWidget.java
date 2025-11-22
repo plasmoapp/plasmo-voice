c365b01039d2d37e77fa2eaa2562ba07d52676dd
Rinary <rinary.super@gmail.com>
2025-11-22 13:15:49 +0200

    move to input device category

--- client/src/main/java/su/plo/voice/client/gui/settings/tab/DevicesTabWidget.java
+++ client/src/main/java/su/plo/voice/client/gui/settings/tab/DevicesTabWidget.java
@@ -69,6 +69,18 @@ public final class DevicesTabWidget extends TabWidget {
                 config.getVoice().getNoiseSuppression()
         ));
         addEntry(createStereoCaptureEntry());
+        addEntry(createToggleEntry(
+                McTextComponent.translatable("gui.plasmovoice.devices.disable_input_device"),
+                McTextComponent.translatable("gui.plasmovoice.devices.disable_input_device.tooltip"),
+                config.getVoice().getDisableInputDevice(),
+                toggled -> {
+                    try {
+                        devices.getInputDevice().ifPresent(InputDevice::close);
+                    } catch (Exception e) {
+                        BaseVoice.LOGGER.error("Failed to disable input device", e);
+                    }
+                }
+        ));
 
         addEntry(new CategoryEntry(McTextComponent.translatable("gui.plasmovoice.devices.output")));
         addEntry(createOutputDeviceEntry());
@@ -94,18 +106,6 @@ public final class DevicesTabWidget extends TabWidget {
                 config.getVoice().getDirectionalSources()
         ));
         addEntry(createHrtfEntry());
-        addEntry(createToggleEntry(
-                McTextComponent.translatable("gui.plasmovoice.devices.disable_input_device"),
-                McTextComponent.translatable("gui.plasmovoice.devices.disable_input_device.tooltip"),
-                config.getVoice().getDisableInputDevice(),
-                toggled -> {
-                    try {
-                        devices.getInputDevice().ifPresent(InputDevice::close);
-                    } catch (Exception e) {
-                        BaseVoice.LOGGER.error("Failed to disable input device", e);
-                    }
-                }
-        ));
     }
 
     @EventSubscribe
