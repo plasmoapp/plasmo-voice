35943a726ad4985b065acfc3bcfde55d01f1c814
Apehum <apehum@proton.me>
2026-06-02 01:26:11 +0800

    feat(client): fabric 26.2-pre-2

--- client/src/main/java/su/plo/voice/client/mixin/MixinIntegratedServer.java
+++ client/src/main/java/su/plo/voice/client/mixin/MixinIntegratedServer.java
@@ -1,7 +1,6 @@
 package su.plo.voice.client.mixin;
 
 import net.minecraft.client.server.IntegratedServer;
-import net.minecraft.world.level.GameType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,7 +11,7 @@ import su.plo.voice.server.ModVoiceServer;
 public class MixinIntegratedServer {
 
     @Inject(method = "publishServer", at = @At("RETURN"))
-    private void publishServer(GameType gameType, boolean allowCheats, int publishPort, CallbackInfoReturnable<Boolean> cir) {
+    private void publishServer(CallbackInfoReturnable<Boolean> cir) {
         boolean published = cir.getReturnValue();
         if (!published) return;
         if (ModVoiceServer.INSTANCE.getConfig().host().port() != 0) return;
