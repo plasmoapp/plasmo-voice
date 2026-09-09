26590c0138a341c9401adf7ab680325ee6883cbc
Apehum <apehum@proton.me>
2026-09-09 17:52:06 +0800

    feat(client): fabric 26.3-pre-3

--- client/src/main/java/su/plo/lib/mod/client/MinecraftUtil.java
+++ client/src/main/java/su/plo/lib/mod/client/MinecraftUtil.java
@@ -5,11 +5,20 @@ import net.minecraft.SharedConstants;
 import net.minecraft.Util;
 import org.jetbrains.annotations.NotNull;
 
+//#if MC>=26.3
+//$$ import com.mojang.blaze3d.Blaze3D;
+//$$ import java.net.URI;
+//#endif
+
 @UtilityClass
 public class MinecraftUtil {
 
     public static void openUri(@NotNull String url) {
+        //#if MC>=26.3
+        //$$ Blaze3D.openUri(URI.create(url));
+        //#else
         Util.getPlatform().openUri(url);
+        //#endif
     }
 
     public static String getVersion() {
